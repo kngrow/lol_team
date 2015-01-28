e782bff760c50d1c960119caab76126cb6748c83
yusuke Yama <ymc9.knit.p23@gmail.com>
2015-01-29 02:28:36 +0900

    片方できた

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-$('.container').basicTabs();
+$(function(){
+  function random(array, num) {
+    var a = array;
+    var t = [];
+    var r = [];
+    var l = a.length;
+    var n = num < l ? num : l;
+    while (n-- > 0) {
+      var i = Math.random() * l | 0;
+      r[n] = t[i] || a[i];
+      --l;
+      t[i] = t[l] || a[l];
+    }
+    return r;
+  }
+  $(document).foundation();
+var lane = ['top','jungle','mid','ADC','support'];
+  $('#randam_start').on('click',function(){
+    var Namearraylist = $('#randam_summoner_list').val().split('\n');
+      if( Namearraylist.length === 5 || Namearraylist.length === 6 ){
+        var Namearray  = random(Namearraylist , 5);
+        var randomlane = random(lane,5);
+        console.log(Namearray);
+        for(var i = 0 ; i<5 ;i++){
+          $('.result_ramdom').append(Namearray[i] + "→"+ randomlane[i] + "<br>");
+          // console.log(Namearray[i] + "→"+ randomlane[i]);
+        }
+      }
+  });
+
+});
